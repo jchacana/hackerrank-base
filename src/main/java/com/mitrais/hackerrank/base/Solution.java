6efbdb0a386a8f346d916e0d4445537f26162036
Javier Chacana <javierchacana@gmail.com>
2018-10-02 21:39:55 +0800

    First commit

---
+++ src/main/java/com/mitrais/hackerrank/base/Solution.java
@@ -0,0 +1,11 @@
+package com.mitrais.hackerrank.base;
+
+/**
+ * Created by jchacana on 10/1/18.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Main class");
+    }
+}
